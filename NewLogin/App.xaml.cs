3d43c4166c9d514120413196be0b311a5b2bc233
roninvictus <A00254959@mycambrian.ca>
2023-05-30 20:18:48 -0400

    first commit

---
+++ NewLogin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NewLogin;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ NewLogin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NewLogin;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
